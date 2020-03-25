4ef677019acf95a69ffae737f8c1d3d2b2aaf4d0
spusina1 <spusina1@etf.unsa.ba>
2020-03-25 17:34:14 +0100

    Product model implementation

---
+++ src/main/java/ba/unsa/etf/si/local_server/models/Product.java
@@ -0,0 +1,89 @@
+package ba.unsa.etf.si.local_server.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.awt.*;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotBlank
+    private String title;
+
+    //private Image image;
+    private Integer quantity;
+    @NotBlank
+    private Float price;
+    private Float discount;
+
+    @ManyToOne
+    @JoinColumn
+    private Branch branch;
+
+
+    public Product(@NotBlank String title, Integer quantity, @NotBlank Float price, Float discount) {
+        this.title = title;
+        //this.image = image;
+        this.quantity = quantity;
+        this.price = price;
+        this.discount = discount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+//    public Image getImage() {
+//        return image;
+//    }
+//
+//    public void setImage(Image image) {
+//        this.image = image;
+//    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Float discount) {
+        this.discount = discount;
+    }
+}
