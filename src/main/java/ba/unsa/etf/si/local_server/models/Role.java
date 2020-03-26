36dd49fe815b5f20a5ff293a28d6f9f74ff451e5
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-03-26 06:52:23 +0100

    Added roles model

---
+++ src/main/java/ba/unsa/etf/si/local_server/models/Role.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.si.local_server.models;
+
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private RoleName name;
+}
--- src/main/java/ba/unsa/etf/si/local_server/models/User.java
+++ src/main/java/ba/unsa/etf/si/local_server/models/User.java
@@ -4,6 +4,8 @@ import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "users", uniqueConstraints = {
@@ -20,10 +22,20 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     @NotBlank
     private String username;
+
     @NotBlank
     private String password;
+
     @NotBlank
     private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
 }
