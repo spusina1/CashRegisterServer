ac379585d19ef4ebf442ae048de16d2f4101006f
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-03-26 06:00:24 +0100

    Removed getters and setters from User model

--- src/main/java/ba/unsa/etf/si/local_server/models/User.java
+++ src/main/java/ba/unsa/etf/si/local_server/models/User.java
@@ -26,42 +26,4 @@ public class User {
     private String password;
     @NotBlank
     private String email;
-
-    public User(@NotBlank String username, @NotBlank String password, @NotBlank String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
