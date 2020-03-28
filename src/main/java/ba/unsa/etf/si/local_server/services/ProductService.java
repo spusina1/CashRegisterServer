433fefcb833cecc2145a44e90c19e2ef3074ef10
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-03-28 08:05:53 +0100

    Removed extra if

--- src/main/java/ba/unsa/etf/si/local_server/services/ProductService.java
+++ src/main/java/ba/unsa/etf/si/local_server/services/ProductService.java
@@ -24,9 +24,6 @@ public class ProductService {
         List<Product> products = productRepository.findAll();
         Stream<Product> productStream = products.stream();
 
-        if(filterRequest.getDiscount()) {
-        }
-
         return productStream.collect(Collectors.toList());
     }
 
