194e91c8805944601857101235ea4edbe023a2c7
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-03-28 08:40:16 +0100

    Added allowed origins on UserController

--- src/main/java/ba/unsa/etf/si/local_server/controllers/UserController.java
+++ src/main/java/ba/unsa/etf/si/local_server/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class UserController {
