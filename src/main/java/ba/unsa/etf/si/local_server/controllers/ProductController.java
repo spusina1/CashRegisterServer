7456b6d3dc31ea472e6df6aa86406261a35c56a2
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-03-28 07:59:19 +0100

    Fixed post mapping url

--- src/main/java/ba/unsa/etf/si/local_server/controllers/ProductController.java
+++ src/main/java/ba/unsa/etf/si/local_server/controllers/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -30,7 +29,7 @@ public class ProductController {
         return productService.getProduct(id);
     }
 
-    @GetMapping("api/main")
+    @GetMapping("/api/main")
     public String getUserFromMain() {
         return productService.getFromMain();
     }
