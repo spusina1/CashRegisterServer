08f98ad73ed679f137b6ef9f0a73b2b3bc43afce
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-03-30 10:56:51 +0200

    Added cron job for sync-up

--- src/main/java/ba/unsa/etf/si/local_server/services/MainSyncUpService.java
+++ src/main/java/ba/unsa/etf/si/local_server/services/MainSyncUpService.java
@@ -3,18 +3,19 @@ package ba.unsa.etf.si.local_server.services;
 import ba.unsa.etf.si.local_server.exceptions.AppException;
 import ba.unsa.etf.si.local_server.exceptions.ResourceNotFoundException;
 import ba.unsa.etf.si.local_server.models.Product;
-import ba.unsa.etf.si.local_server.models.Role;
 import ba.unsa.etf.si.local_server.models.User;
 import ba.unsa.etf.si.local_server.requests.MainLoginRequest;
 import ba.unsa.etf.si.local_server.responses.MainLoginResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -25,37 +26,41 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Service
 public class MainSyncUpService {
-
     private final RestTemplate restTemplate;
-    private final String baseURI = "https://main-server-si.herokuapp.com/api";
-    private final MainLoginRequest mainLoginRequest = new MainLoginRequest("office1", "password");
 
-    public String obtainOfficeManagerToken(MainLoginRequest request) {
-        try {
-            String uri = baseURI + "/auth/login";
-            return restTemplate.postForObject(uri, request, MainLoginResponse.class).getToken();
-        } catch (HttpClientErrorException err) {
-            throw new ResourceNotFoundException("Could not get login response from the main server");
-        }
+    @Value("${app.mainServerBaseURI}")
+    private String baseURI;
+
+    @Value("${app.mainUsername}")
+    private String username;
+
+    @Value("${app.mainPassword}")
+    private String password;
+
+
+    @Scheduled(cron = "${cron.mainFetch}")
+    public void syncDatabases() {
+        System.out.println("Synchronizing databases...");
     }
 
-    public List<Product> syncProductsFromMain() {
+    private List<Product> fetchProductsFromMain() {
         int officeID = 1;
         String uri = String.format("%s/offices/%d/products", baseURI, officeID);
         String json = makeGetRequest(uri);
         return jsonListToObjectList(json, this::mapJsonToProduct);
     }
 
-    public List<User> syncUsersFromMain() {
+    private List<User> fetchUsersFromMain() {
         String uri = String.format("%s/office-employees", baseURI);
         String json = makeGetRequest(uri);
         return jsonListToObjectList(json, this::mapJsonToUser);
     }
 
     private String makeGetRequest(String uri) {
+        MainLoginRequest mainLoginRequest = new MainLoginRequest(username, password);
         String token = obtainOfficeManagerToken(mainLoginRequest);
 
         HttpHeaders headers = new HttpHeaders() {{
@@ -65,6 +70,15 @@ public class MainSyncUpService {
 
         ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<>(headers), String.class);
         return response.getBody();
+    }
+
+    private String obtainOfficeManagerToken(MainLoginRequest request) {
+        try {
+            String uri = baseURI + "/auth/login";
+            return restTemplate.postForObject(uri, request, MainLoginResponse.class).getToken();
+        } catch (HttpClientErrorException err) {
+            throw new ResourceNotFoundException("Could not get login response from the main server");
+        }
     }
 
     private <T> ArrayList<T> jsonListToObjectList(String json, Function<JsonNode, T> mapJsonNodeToObject) {
