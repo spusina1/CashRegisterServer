9940a67c6d6cca3c0c6cd3ceed44e31f54d234a8
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-03-30 20:58:59 +0200

    Fixed products issue

--- src/main/java/ba/unsa/etf/si/local_server/controllers/UserController.java
+++ src/main/java/ba/unsa/etf/si/local_server/controllers/UserController.java
@@ -5,7 +5,10 @@ import ba.unsa.etf.si.local_server.requests.LoginRequest;
 import ba.unsa.etf.si.local_server.responses.LoginResponse;
 import ba.unsa.etf.si.local_server.security.CurrentUser;
 import ba.unsa.etf.si.local_server.security.UserPrincipal;
+import ba.unsa.etf.si.local_server.services.MainSyncUpService;
 import ba.unsa.etf.si.local_server.services.UserService;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonFactoryBuilder;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +21,7 @@ import javax.validation.Valid;
 @RequestMapping("/api")
 public class UserController {
     private final UserService userService;
+    private final MainSyncUpService mainSyncUpService;
 
     @GetMapping("/test")
     public String result() {
@@ -34,6 +38,12 @@ public class UserController {
     public ResponseEntity<?> getUserProfile(@CurrentUser UserPrincipal userPrincipal) {
         User user = userService.getUserByUsername(userPrincipal.getUsername());
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/sync")
+    public ResponseEntity<?> syncDatabases() {
+        mainSyncUpService.syncDatabases();
+        return ResponseEntity.ok("Synced...");
     }
 
 }
