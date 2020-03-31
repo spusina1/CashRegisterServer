48e1e624c36da368843579a4908e52af52b4c5a0
spusina1 <spusina1@etf.unsa.ba>
2020-03-31 22:59:12 +0200

    Receipt model

---
+++ src/main/java/ba/unsa/etf/si/local_server/models/Receipt.java
@@ -0,0 +1,38 @@
+package ba.unsa.etf.si.local_server.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "receipts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Receipt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String receiptId;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "receipt_receiptItem",
+            joinColumns = @JoinColumn(name = "receipt_id"),
+            inverseJoinColumns = @JoinColumn(name = "receiptItem_id"))
+    private Set<ReceiptItem> receiptItems = new HashSet<>();
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private  BigDecimal totalPrice;
+
+}
