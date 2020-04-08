066dfe7f38d9c4455415360027c52bfe89be9452
spusina1 <spusina1@etf.unsa.ba>
2020-04-08 14:31:25 +0200

    added deleteByReceipt

--- src/main/java/ba/unsa/etf/si/local_server/repositories/ReceiptItemRepository.java
+++ src/main/java/ba/unsa/etf/si/local_server/repositories/ReceiptItemRepository.java
@@ -3,8 +3,22 @@ package ba.unsa.etf.si.local_server.repositories;
 import ba.unsa.etf.si.local_server.models.transactions.Receipt;
 import ba.unsa.etf.si.local_server.models.transactions.ReceiptItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface ReceiptItemRepository  extends JpaRepository<ReceiptItem, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(
+            nativeQuery = true,
+            value = "delete from receipt_items where receipt_id = ?1"
+    )
+    void deleteByReceipt(Long id);
 }
