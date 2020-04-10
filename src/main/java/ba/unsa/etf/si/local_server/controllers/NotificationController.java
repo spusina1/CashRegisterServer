03f7e728a9214b2c2219324be5a95cf09532a685
SamraMujcinovic <44182000+SamraMujcinovic@users.noreply.github.com>
2020-04-10 10:44:49 +0200

    Call the waiter (#43)
    * implemented route /api/notifications
    
    * implemented route api/notifications/{id}
    
    Co-authored-by: azigo12 <azigo1@etf.unsa.ba>

--- src/main/java/ba/unsa/etf/si/local_server/controllers/NotificationController.java
+++ src/main/java/ba/unsa/etf/si/local_server/controllers/NotificationController.java
@@ -1,11 +1,31 @@
 package ba.unsa.etf.si.local_server.controllers;
 
+import ba.unsa.etf.si.local_server.models.Notification;
+import ba.unsa.etf.si.local_server.requests.SellerAppRequest;
+import ba.unsa.etf.si.local_server.requests.SendNotificationRequest;
+import ba.unsa.etf.si.local_server.responses.SaveNotificationResponse;
 import ba.unsa.etf.si.local_server.services.NotificationService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 public class NotificationController {
     private final NotificationService notificationService;
+
+    @PostMapping("/api/notifications")
+    public ResponseEntity<?> saveNotification(@Valid @RequestBody SendNotificationRequest notificationRequest){
+        String responseMessage = notificationService.saveNotification(notificationRequest);
+        return ResponseEntity.ok(new SaveNotificationResponse(responseMessage));
+    }
+
+    @GetMapping("/api/notifications/{id}")
+    public List<Notification> getNotifications(@PathVariable Long id) {
+        return notificationService.getNotifications(id);
+    }
+
 }
--- src/main/java/ba/unsa/etf/si/local_server/services/NotificationService.java
+++ src/main/java/ba/unsa/etf/si/local_server/services/NotificationService.java
@@ -1,13 +1,38 @@
 package ba.unsa.etf.si.local_server.services;
 
+import ba.unsa.etf.si.local_server.models.Notification;
 import ba.unsa.etf.si.local_server.repositories.NotificationRepository;
 import ba.unsa.etf.si.local_server.repositories.ProductRepository;
+import ba.unsa.etf.si.local_server.requests.SellerAppRequest;
+import ba.unsa.etf.si.local_server.requests.SendNotificationRequest;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class NotificationService {
     private final NotificationRepository notificationRepository;
+
+    public String saveNotification(SendNotificationRequest notificationRequest) {
+        if(notificationRequest != null){
+            Notification newNotification = new Notification();
+            newNotification.setMessage(notificationRequest.getMessage());
+            notificationRepository.save(newNotification);
+            return "Notification is successfully saved!";
+        }
+        return "";
+    }
+
+    public List<Notification> getNotifications(Long id) {
+        return notificationRepository
+                .findAll()
+                .stream()
+                .filter(n -> n.getId() > id)
+                .collect(Collectors.toList());
+    }
+
 }
