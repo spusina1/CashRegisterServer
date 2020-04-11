c771144088d4b6673c6727986d2bba81c6898816
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-04-11 12:10:22 +0200

    Implemented local guest user

--- src/main/java/ba/unsa/etf/si/local_server/services/UserService.java
+++ src/main/java/ba/unsa/etf/si/local_server/services/UserService.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.si.local_server.services;
 
 import ba.unsa.etf.si.local_server.exceptions.ResourceNotFoundException;
+import ba.unsa.etf.si.local_server.models.Role;
+import ba.unsa.etf.si.local_server.models.RoleName;
 import ba.unsa.etf.si.local_server.models.User;
 import ba.unsa.etf.si.local_server.repositories.RoleRepository;
 import ba.unsa.etf.si.local_server.repositories.UserRepository;
@@ -11,9 +13,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 
 @AllArgsConstructor
 @Service
@@ -22,6 +26,7 @@ public class UserService {
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public String authenticateUser(LoginRequest loginRequest) {
         Authentication authentication = authenticationManager.authenticate(
@@ -43,11 +48,40 @@ public class UserService {
     }
 
     public void batchInsertUsers(List<User> users) {
+        Long guestId = users
+                .stream()
+                .map(User::getId)
+                .min(Comparator.comparingLong(id -> id))
+                .orElse(1L);
+
         userRepository.deleteAllInBatch();
         userRepository.flush();
         roleRepository.deleteAllInBatch();
         roleRepository.flush();
         userRepository.saveAll(users);
+        userRepository.save(createGuestUser(guestId));
         userRepository.flush();
+    }
+
+    public User createGuestUser(Long guestId) {
+        User user = new User(
+                guestId,
+                "guest",
+                "password",
+                "guest@mail.com",
+                null,
+                "Guest",
+                "User",
+                "no address",
+                "no city",
+                "no country",
+                "no phone number");
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        List<Role> userRoles = Collections.singletonList(new Role(0L, RoleName.ROLE_GUEST));
+        user.setRoles(new HashSet<>(userRoles));
+
+        return user;
     }
 }
