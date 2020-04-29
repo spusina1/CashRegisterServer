bcda81ff90097759a0f9a701920cf75a35e9f21e
samra <smujcinovi1@etf.unsa.ba>
2020-04-29 16:35:05 +0200

    Implemented route GET api/tables

--- src/main/java/ba/unsa/etf/si/local_server/services/TableService.java
+++ src/main/java/ba/unsa/etf/si/local_server/services/TableService.java
@@ -1,13 +1,23 @@
 package ba.unsa.etf.si.local_server.services;
 
+import ba.unsa.etf.si.local_server.models.Notification;
+import ba.unsa.etf.si.local_server.models.Table;
 import ba.unsa.etf.si.local_server.repositories.NotificationRepository;
 import ba.unsa.etf.si.local_server.repositories.TableRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class TableService {
     private final TableRepository tableRepository;
 
+    public List<Table> getTables() {
+        return new ArrayList<>(tableRepository
+                .findAll());
+    }
 }
